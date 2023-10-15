b33225935f6d06e92e1179e1379ec57c56bf1d47
lawsonredeye <omoregbeeolawson@gmail.com>
2023-10-15 13:03:17 -0700

    modified the file for the coding style and also removed unused variable p

--- _printf.c
+++ _printf.c
@@ -1,49 +1,50 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * _printf - function that prints
+ * to the standard output
+ * @format: the first arg
+ * Return: 0
+ */
 int _printf(const char *format, ...)
 {
-    int i, j;
-    char next_char;
-    char* string;
-    char cc, p;
-    va_list arg;
+	int i, j;
+	char next_char;
+	char *string;
+	char cc;
+	va_list arg;
 
-    va_start(arg, format);
-    for (i = 0; format[i] != '\0'; i++)
-    {
-        if (format[i] == '%')
-        {
-            next_char = format[i + 1];
-            if (next_char == 's')
-            {
-               string = va_arg(arg, char *);
-               for (j = 0; string[j] != '\0'; j++)
-               {
-                   putchar(string[j]);
-               }
-               i++;
-            }
-            else if (next_char == 'c')
-            {
-                cc = va_arg(arg, int);
-                putchar(cc);
-                i++;
-            }
-            else if (next_char == '%')
-            {
-                putchar('%');
-                i++;
-            }
-        }
-        else
-            putchar(format[i]);
-    }
-    va_end(arg);
-    return (0);
+	va_start(arg, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			next_char = format[i + 1];
+			if (next_char == 's')
+			{
+				string = va_arg(arg, char *);
+				for (j = 0; string[j] != '\0'; j++)
+				{
+					putchar(string[j]);
+				}
+				i++;
+			}
+			else if (next_char == 'c')
+			{
+				cc = va_arg(arg, int);
+				putchar(cc);
+				i++;
+			}
+			else if (next_char == '%')
+			{
+				putchar('%');
+				i++;
+			}
+		}
+		else
+			putchar(format[i]);
+	}
+	va_end(arg);
+	return (0);
 }
-int main(void)
-{
-    _printf("%%");
-}
-
