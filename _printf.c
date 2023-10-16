c657dfa51b00a0770d40de6a41cb26cbb1473ff2
avonggeorge <georgeavong@yahoo.com>
2023-10-16 01:11:53 -0700

    Proper indentation and addition of header library

--- _printf.c
+++ _printf.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include "main.h"
 
 /**
  * _printf - prints to the standard output
@@ -8,58 +9,58 @@
  */
 int _printf(const char *format, ...)
 {
-        int i, j, count = 0;
-        char next_char, cc;
-        char *string;
-        va_list arg;
+	int i, j, count = 0;
+	char next_char, cc;
+	char *string;
+	va_list arg;
 
-        if (format == NULL)
-                return (-1);
-        va_start(arg, format);
-        for (i = 0; format[i] != '\0'; i++)
-        {
-            if (format[i] == '\n')
-            {
-                putchar('\n');
-                    i++;
-                    count++;
-                }
-            if (format[i] == '%')
-                {
-                        next_char = format[i + 1];
-                        if (next_char == 's')
-                        {
-                                string = va_arg(arg, char *);
-                                if (string == NULL)
-                                        return (-1);
-                                for (j = 0; string[j] != '\0'; j++)
-                                {
-                                        putchar(string[j]);
-                                        count++;
-                                }
-                                i++;
-                        }
-                        else if (next_char == 'c')
-                        {
-                                cc = va_arg(arg, int);
-                                putchar(cc);
-                                i++;
-                                count++;
-                        }
-                        else if (next_char == '%')
-                        {
-                                putchar('%');
-                                i++;
-                                count++;
-                        }
-                }
-                else
-                {
-                        putchar(format[i]);
-                        count++;
-                }
-        }
-        va_end(arg);
-        return (count);
+	if (format == NULL)
+		return (-1);
+	va_start(arg, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '\n')
+		{
+			putchar('\n');
+			i++;
+			count++;
+		}
+		if (format[i] == '%')
+		{
+			next_char = format[i + 1];
+			if (next_char == 's')
+			{
+				string = va_arg(arg, char *);
+				if (string == NULL)
+					return (-1);
+				for (j = 0; string[j] != '\0'; j++)
+				{
+					putchar(string[j]);
+					count++;
+				}
+				i++;
+			}
+			else if (next_char == 'c')
+			{
+				cc = va_arg(arg, int);
+				putchar(cc);
+				i++;
+				count++;
+			}
+			else if (next_char == '%')
+			{
+				putchar('%');
+				i++;
+				count++;
+			}
+		}
+		else
+		{
+			putchar(format[i]);
+			count++;
+		}
+	}
+	va_end(arg);
+	return (count);
 }
 
