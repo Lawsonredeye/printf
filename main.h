8467acb09188c34aa89317b9631f3765d4b10dcd
lawsonredeye <omoregbeeolawson@gmail.com>
2023-10-17 17:24:45 -0700

    updated main header file

--- main.h
+++ main.h
@@ -2,4 +2,7 @@
 #define MAIN_H
 int _printf(const char *format, ...);
 void handle_b(int my_integer, int *count);
+void hexadec(int hexa, int *count, bool isUppercase);
+void octconversion(int octnum, int *count);
+void unsign_Only(unsigned int nums, int *count);
 #endif
