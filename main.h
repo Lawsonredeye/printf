302df38397adf4338bc7b01e57bc5600cb54b3ee
avonggeorge <georgeavong@yahoo.com>
2023-10-18 05:11:30 -0700

    add buffer prototype

--- main.h
+++ main.h
@@ -7,4 +7,5 @@ void handle_b(int my_integer, int *count);
 void hexadec(int hexa, int *count, bool isUppercase);
 void octconversion(int octnum, int *count);
 void unsign_Only(unsigned int nums, int *count);
+void fine_print(char mychar, FILE* myfile);
 #endif
